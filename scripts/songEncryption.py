419920221200f9792453452f3f731606d56e2221
Patricia <pattid1608@gmail.com>
2024-12-07 00:43:22 -0500

    made it give me a random song using the song id

--- scripts/songEncryption.py
+++ scripts/songEncryption.py
@@ -1,9 +1,15 @@
-
+import random
 from lyricsgenius import Genius
 
-genius = Genius("ytTQBFpeW9m1wOxySiu-h1mIJ_0qiv2E52oRSqoYR8NV5wE9Xka-Ngh2ugLUU88y")
-artist = genius.search_artist("Chappell Roan", max_songs=10, include_features=False)
-print(artist.songs)
+nbr = random.randint(1, 249990)
+print(nbr)
 
-song = artist.song("Casual")
+genius = Genius("ytTQBFpeW9m1wOxySiu-h1mIJ_0qiv2E52oRSqoYR8NV5wE9Xka-Ngh2ugLUU88y")
+#artist = genius.search_artist("Chappell Roan", max_songs=10, include_features=False)
+#print(artist.songs)
+
+song = genius.search_song(song_id=nbr)
+
+print(song.title)
+print(song.artist)
 print(song.lyrics)
